960fa1de12aa35c02b29e02bbc09e1214f581dc5
Arkadipta <gogobuddy.cool@gmail.com>
2020-04-26 01:24:50 +0530

    Working config as of 20200426

---
+++ elpa/conda-20200120.1721/conda-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "conda" "20200120.1721" "Work with your conda environments" '((emacs "24.4") (pythonic "0.1.0") (dash "2.13.0") (s "1.11.0") (f "0.18.2")) :commit "335474e409774e31125fe320a4a54c8d92bf5fa2" :keywords '("python" "environment" "conda") :authors '(("Rami Chowdhury" . "rami.chowdhury@gmail.com")) :maintainer '("Rami Chowdhury" . "rami.chowdhury@gmail.com") :url "http://github.com/necaris/conda.el")
